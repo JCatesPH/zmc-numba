8485512b52c699f13b91719255230af1d4081957
JCatesPH <jmcates@crimson.ua.edu>
2019-06-17 12:34:58 -0500

    Speed up?

--- modimod_v3.py
+++ modimod_v3.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 import time
 import numpy as np
@@ -10,6 +11,13 @@ lattice = 1.420000000 * 10 ** (-10)
 t = 4.8000000 * 10 ** (-19)
 eta = 1.0000000 * 10 ** (-3)
 
+
+sq3 = math.sqrt(float(3))
+sq3pi = sq3 * math.pi
+pi2 = 2 * math.pi
+
+qx = 0.1 / pi2
+qy = 0
 
 def setom(ome):
     global om
@@ -45,36 +53,31 @@ def integrand(x):
     # qy=[]
 
     # In PRB 
-    qx = 0.1 / (2 * math.pi)
-    qy = 0
-    sq3 = math.sqrt(float(3))
-
-    sq3x1 = sq3 * x[1]
-    sq3x1pi = sq3x1 * math.pi
-    phi = x[0] * 2 * math.pi
-    tau = sq3x1 * 2 * math.pi / 2
-    rho = phi / 2
-    psi = x[0] + qx
-    eta = psi * 2 * math.pi
-    zeta = x[1] + qy
-    sq3zetapi = sq3 * math.pi * zeta
-
-    nu = eta / 2 - sq3 * (zeta) * 2 * math.pi / 2
-    mu = eta / 2 + sq3 * (zeta) * 2 * math.pi / 2
 
 
     om = getom()
 
-    hkxky = exi(phi, 1) * (exi((rho - tau), 1) + exi((rho + tau), 1) + exi(phi, -1))
-    chkxky = exi(phi, -1) * (exi((rho - tau), -1) + exi((rho + tau), -1) + exi(phi, 1))
+    hkxky = exi(x[0] * pi2, 1) * (
+                exi((x[0] * math.pi - sq3pi * x[1]), 1) + exi(
+            (x[0] * math.pi + sq3pi * x[1]), 1) + exi((x[0] * pi2), -1))
+    chkxky = exi(x[0] * pi2, -1) * (
+                exi((x[0] * math.pi - sq3pi * x[1]), -1) + exi(
+            (x[0] * math.pi + sq3pi * x[1]), -1) + exi((x[0] * pi2), 1))
+    hkxqxky = exi((x[0] + qx) * pi2, 1) * (
+                exi(((x[0] + qx) * math.pi - sq3pi * (x[1] + qy)), 1) + exi(
+            ((x[0] + qx) * math.pi + sq3pi * (x[1] + qy)), 1) + exi(
+            ((x[0] + qx) * pi2), -1))
+    chkxqxky = exi((x[0] + qx) * pi2, -1) * (
+                exi(((x[0] + qx) * math.pi - sq3pi * (x[1] + qy)), -1) + exi(
+            ((x[0] + qx) * math.pi + sq3pi * (x[1] + qy)), -1) + exi(
+            ((x[0] + qx) * pi2), 1))
 
-    hkxqxky = exi(eta, 1) * (exi(nu, 1) + exi(mu, 1) + exi(eta, -1))
-    chkxqxky = exi(eta, -1) * (exi(nu, -1) + exi(mu, -1) + exi(eta, 1))
-
-    ekp = math.sqrt(3 + 2 * math.cos(sq3x1pi * 2) + 4 * math.cos(sq3x1pi) * math.cos(3 * x[0] * math.pi))
+    ekp = math.sqrt(
+        3 + 2 * math.cos(sq3pi * x[1] * 2) + 4 * math.cos(sq3pi * x[1]) * math.cos(
+            3 * x[0] * math.pi))
     ekm = -ekp
-
-    ekqp = math.sqrt(3 + 2 * math.cos(sq3zetapi * 2) + 4 * math.cos(sq3zetapi) * math.cos(3 * psi * math.pi))
+    ekqp = math.sqrt(3 + 2 * math.cos(sq3 * (x[1] + qy) * pi2) + 4 * math.cos(
+        sq3pi * (x[1] + qy)) * math.cos(3 * (x[0] + qx) * math.pi))
     ekqm = -ekqp
 
     hp = (chkxky * hkxqxky / (ekp * ekqp) + 1) * (hkxky * chkxqxky / (ekp * ekqp) + 1) / 4
@@ -85,54 +88,54 @@ def integrand(x):
     a3 = hm * (fermi(ekp) - fermi(ekqm)) / (ekp - ekqm + 1j * eta + om)
     a4 = hp * (fermi(ekp) - fermi(ekqp)) / (ekp - ekqp + 1j * eta + om)
 
-    a = (a1 + a2 + a3 + a4) / (2 * math.pi) ** 2
+    a = (a1 + a2 + a3 + a4) / (pi2) ** 2
 
     return a.imag
 
 
-##############################################################################
-# # Defining calculation parameters and printing them
+###################################################################
+# Setting parameters and printing them
+
+depths = 3
+sigmults = 100
+trials = 10
+
+print('\n========================================================\n')
+
+print(__file__)
+print(datetime.date.today())
+
+print('\ndepth = ', depths)
+print('sigma_multiplication = ', sigmults)
+print('num_trials = ', trials)
+
 kxi = -1 / 3
 kxf = 1 / 3
 
 kyi = -0.1925
 kyf = 0.385
 
+print('\nkxi = ', kxi)
+print('kxf = ', kxf)
+
+print('\nkyi = ', kyi)
+print('kyf = ', kyf)
+
 beg = 1
 end = 51
 spacing = 50
 
-depths = 3
-sigmults = 100
-trials = 10
+print('\nlinspace parameters: %3f,%3f,%3f' % (beg, end, spacing))
 
-print('Following values are constant for all integrations.')
-print('\n========================================================')
-
-print('\ndepth = ', depths)
-print('sigma_multiplication = ', sigmults)
-print('num_trials = ', trials)
-
-print('\nkxi = ', kxi)
-print('kxf = ', kxf)
-print('kyi = ', kyi)
-print('kyf = ', kyf)
-
-print('\nLinspace parameters : %3f, %3f, %3f' % (beg, end, spacing))
-print('========================================================')
-
-tic = time.time()
-
+print('=======================================================')
 resultArr = np.zeros(spacing)
 errorArr = np.zeros(spacing)
 timeArr = np.zeros(spacing)
 
-##############################################################################
-
-
+tic = time.time()
 j = 0
 for i in np.linspace(beg, end, spacing):
-    setom(1.98 + i * 0.05 / (50))
+    setom(1.98 + i * 0.05 / spacing)
     MC = ZMCIntegral.MCintegral(integrand, [[kxi, kxf], [kyi, kyf]])
     # Setting the zmcintegral parameters
     MC.depth = depths
@@ -141,7 +144,7 @@ for i in np.linspace(beg, end, spacing):
     start = time.time()
     result = MC.evaluate()
     print('================================================================')
-    print('Result for r = ', i, ': ', result[0], ' with error: ', result[1])
+    print('Result for om = ', 1.98 + i * 0.05 / spacing, ': ', result[0], ' with error: ', result[1])
     print('================================================================')
     end = time.time()
     print('Computed in ', end - start, ' seconds.')
@@ -151,13 +154,13 @@ for i in np.linspace(beg, end, spacing):
     timeArr[j] = end - start
     j = j + 1
 
-print('================================================================')
+print('\n\n================================================================')
 
 j = 0
 print('All values in csv format:')
-print('r,Integral,Error,Time')
+print('om,Integral,Error,Time')
 for i in np.linspace(beg, end, spacing):
-    print('%5.3f, %11.8E, %5.3E, %5.3E' % (i, resultArr[j], errorArr[j], timeArr[j]))
+    print('%5.3f, %11.8E, %5.3E, %5.3E' % (1.98 + i * 0.05 / spacing, resultArr[j], errorArr[j], timeArr[j]))
     j = j + 1
 
 toc = time.time()
