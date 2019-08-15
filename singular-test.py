3a519349b989738bb231f0159ae5a1891273b4bb
JCatesPH <jmcates@crimson.ua.edu>
2019-08-15 14:03:21 -0500

    See zmc result for integral tested with gvegas

---
+++ singular-test.py
@@ -0,0 +1,31 @@
+import math
+import time
+from numba import cuda
+from ZMCintegral import ZMCintegral
+
+# user defined function
+@cuda.jit(device=True)
+def my_func(x):
+    value = 0
+    for i in range(5):
+        value = value + math.cos(math.log(x[i]) / x[i]) / x[i]
+    return value
+
+MC = ZMCintegral.MCintegral(my_func,[[0,1],[0,1],[0,1],[0,1],[0,1]])
+
+MC.depth = 2
+MC.sigma_multiplication = 5
+MC.num_trials = 5
+
+
+start = time.time()
+# obtaining the result
+result = MC.evaluate()
+
+end = time.time()
+
+# print the formatted result
+print('result = %s    std = %s' % (result[0], result[1]))
+
+
+print('Time to calculate: %5.4f s' % (end-start))
